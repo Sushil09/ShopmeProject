0c6fb0b2d2a9ce3c84a7bb994a5d11181fe21321
Sushil Jaiswal <sushil.jaiswal@oracle.com>
2022-04-02 16:54:24 +0530

    Adding sorting func. to Admin homepage.

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/controller/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/controller/UserController.java
@@ -7,6 +7,7 @@ import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -28,12 +29,17 @@ public class UserController {
 
     @GetMapping("/users")
     public String listAll(Model model) {
-        return listByPage(1, model);
+        return listByPage(1, model, "firstName", "asc");
     }
 
     @GetMapping("/users/page/{pageNum}")
-    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
-        Page<User> page = userService.listByPage(pageNum);
+    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
+                             @Param("sortField") String sortField, @Param("sortDir") String sortDir
+    ) {
+        System.out.println("Sort Field: " + sortField);
+        System.out.println("Sort Order: " + sortDir);
+
+        Page<User> page = userService.listByPage(pageNum, sortField, sortDir);
         List<User> listUsers = page.getContent();
 
         long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
@@ -48,6 +54,10 @@ public class UserController {
         model.addAttribute("endCount", endCount);
         model.addAttribute("totalItems", page.getTotalElements());
         model.addAttribute("listUsers", listUsers);
+        String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", reverseSortDir);
 
         return "users";
     }
